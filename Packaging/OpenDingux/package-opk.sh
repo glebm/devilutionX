b0c181464880aee41c8f6bb3c642edb0de021f69
Gleb Mazovetskiy <glex.spb@gmail.com>
2019-10-18 21:41:44 +0100

    RG-350: Update controller mappings for the latest kernel

--- Packaging/OpenDingux/package-opk.sh
+++ Packaging/OpenDingux/package-opk.sh
@@ -11,7 +11,7 @@ echo 1>&2 Packaging ${OUT}...
 
 # 190000006c696e6b6465762064657600 "linkdev device (Analog 2-axis 8-button
 # 2-hat)" axes:4 buttons:8 hats:1 balls:0
-RG350_JOYSTICK=190000006c696e6b6465762064657600,RG350,platform:Linux,x:b2,a:b1,b:b0,y:b3,back:b4,start:b5,dpleft:h0.8,dpdown:h0.4,dpright:h0.2,dpup:h0.1,leftshoulder:b6,rightshoulder:b7,leftx:a0,lefty:a1,rightx:a2,righty:a3,
+RG350_JOYSTICK=190000006c696e6b6465762064657600,RG350,,platform:Linux,x:b2,a:b1,b:b0,y:b3,back:b8,start:b9,dpleft:h0.8,dpdown:h0.4,dpright:h0.2,dpup:h0.1,leftshoulder:b4,lefttrigger:b6,rightshoulder:b5,righttrigger:b7,leftstick:b10,rightstick:b11,leftx:a0,lefty:a1,rightx:a2,righty:a3,
 
 set -x
 rm -rf "${TMP}"
