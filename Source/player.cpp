e449c4dd33457d9493b3a22fb02ec23db978956f
Gleb Mazovetskiy <glex.spb@gmail.com>
2019-11-10 23:09:34 +0800

    devilutionX-QOL-patches/mk_run_in_town_smooth_01.patch

--- Source/player.cpp
+++ Source/player.cpp
@@ -1210,8 +1210,22 @@ void PM_ChangeOffset(int pnum)
 	}
 
 	plr[pnum]._pVar8++;
-	px = plr[pnum]._pVar6 / 256;
-	py = plr[pnum]._pVar7 / 256;
+
+	if (currlevel == 0) // QoL patch
+		plr[pnum]._pVar8++;
+
+	// speedup run animation: //TODO bad place
+	if (currlevel == 0) {
+		if (plr[pnum]._pAnimFrame % 2 == 1) {
+			plr[pnum]._pAnimFrame++;
+		}
+		if (plr[pnum]._pAnimFrame >= plr[pnum]._pWFrames) {
+			plr[pnum]._pAnimFrame = 0;
+		}
+	}
+
+	plr[pnum]._pVar6 += (currlevel == 0 ? 2 : 1) * plr[pnum]._pxvel;
+	plr[pnum]._pVar7 += (currlevel == 0 ? 2 : 1) * plr[pnum]._pyvel;
 
 	plr[pnum]._pVar6 += plr[pnum]._pxvel;
 	plr[pnum]._pVar7 += plr[pnum]._pyvel;
