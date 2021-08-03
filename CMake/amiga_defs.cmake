2d47fe180d6f86744f83e8c01be0fb6ea9ae0628
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-08-03 17:40:13 +0100

    Amiga: Video mode

--- CMake/amiga_defs.cmake
+++ CMake/amiga_defs.cmake
@@ -3,6 +3,8 @@ set(UBSAN OFF)
 set(NONET ON)
 set(USE_SDL1 ON)
 set(SDL1_VIDEO_MODE_BPP 8)
+set(SDL1_VIDEO_MODE_FLAGS SDL_HWSURFACE|SDL_DOUBLEBUF)
+set(SDL1_FORCE_SVID_VIDEO_MODE ON)
 set(TTF_FONT_NAME \"LiberationSerif-Bold.ttf\")
 # Enable exception suport as they are used in dvlnet code
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fexceptions")
